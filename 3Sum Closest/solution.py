164daf833793fedbc5d4e3fb32adc95ad957a3ed
kenilpatel <kenilpatel786@gmail.com>
2021-01-18 12:45:32 -0600

    added 3Sum Closest

---
+++ 3Sum Closest/solution.py
@@ -0,0 +1,25 @@
+import bisect
+
+
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums = sorted(nums)
+        len_nums = len(nums)
+        diff = float('inf')
+        for i in range(0, len_nums):
+            for j in range(i + 1, len_nums):
+                complement = target - nums[i] - nums[j]
+                hi = bisect_right(nums, complement, j + 1)
+                lo = hi - 1
+                if hi < len(nums) and abs(complement - nums[hi]) < abs(diff):
+                    diff = complement - nums[hi]
+                if lo > j and abs(complement - nums[lo]) < abs(diff):
+                    diff = complement - nums[lo]
+                if diff == 0:
+                    break
+        return target - diff
